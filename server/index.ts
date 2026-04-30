8568b1a8f5557631f122310a74f58fc13813ce45
AMAX GLOBAL <info@amaxglobal.com.au>
2026-04-30 22:53:25 +1000

    Baseline: dev env running, db pushed, deps installed

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express, { type Request, Response, NextFunction } from "express";
 import { randomUUID } from "crypto";
 import rateLimit from "express-rate-limit";
